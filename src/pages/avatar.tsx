9bb3fbf04bbcd718f63f97bbad24ba9d5934f7b5
Valarie Wong <valarie.email@gmail.com>
2025-02-15 14:19:47 -0500

    Finished avatar page

--- src/pages/avatar.tsx
+++ src/pages/avatar.tsx
@@ -5,13 +5,34 @@ import "./../styles/avatar.css";
 const Avatar = () => {
   return (
     <>
-      <span className = "titleAvatar"><h1>CHOOSE YOUR AVATAR</h1></span>
+      <h1 className="titleAvatar">CHOOSE YOUR AVATAR</h1>
 
-      <div className="container">
+      <Link to ="/"><img src = "images/setting.png" alt="setting" className="settingButton"></img></Link>
+
+      <div className="pageContainer">
+        <div className="giraffeContainer">
+            <Link to ="/"><img src="images/giraffe.png" alt="giraffe" className="avatarImage" /></Link>
+          
+            <button className="avatarButton"><Link to="/">tanzanian <br /> giraffe</Link></button>
+          
+        </div>
+        
+        <div className="mooseContainer">
+            <Link to="/"><img src="images/moose.png" alt="moose" className="avatarImage" /></Link>
+          
+            <button className="avatarButton"><Link to="/">canadian <br /></Link> moose</button>
+          
+        </div>
+
+        <div className="racoonContainer">
+            <Link to="/"><img src="images/racoon.png" alt="racoon" className="avatarImage" /></Link>
+          
+            <button className="avatarButton"><Link to="/">japanese <br /> racoon</Link></button>
+          
+        </div>
+
         
       </div>
-
-      
     </>
   );
 };
