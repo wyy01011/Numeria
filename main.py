61d11b0caff51accb15e4fe6ac87dc7248635307
AyeshaTayyiba <36958830+AyeshaTayyiba@users.noreply.github.com>
2025-02-15 12:43:43 -0500

    corrected prompt to give list of Qs and num ans

--- main.py
+++ main.py
@@ -30,18 +30,52 @@ data = loader.load()
 
 
 # Define the function of the chatbot
+# template = ("""
+# You are a math teacher. You are teaching students in grade {grade} in {location}.   
+# Based on the Curriculum provided, can you come up with one question in each category in the curriculum.
+# While you read through the curriculum and think about the categories, don't output any of your thoughts.
+# The output should only be the math questions. List each of the questions as a numbered list.
+# Curriculum: {curriculum}
+# """)
+
+
 template = ("""
-You are a math teacher. You are teaching students in grade {grade} in {location}.   
-Based on the Curriculum provided, can you come up with one question in each category in the curriculum.
-While you read through the curriculum and think about the categories, don't output any of your thoughts.
-The output should only be the math questions. List each of the questions as a numbered list.
+"You are a math teacher. You are teaching students in grade {grade} in {location}, specifically following the curriculum provided.
+Please read through the curriculum and generate one math question for each category outlined in the curriculum. 
+The questions should be clear, simple, and appropriate for the students, and they should directly correspond to the categories in the curriculum.
+Do not include any category titles before the questions.
+The answers to the math questions must be numerical. If the answer is not numerical, generate a different question until you have a numerical answer, or exculde the category.
+Provide the questions in a numbered list, with no other content in your response.
+Provide the corresponding answers in a numbered list, with no other content in your response.
+Begin by generating only the questions and answers, one per curriculum category."
 Curriculum: {curriculum}
 """)
 
 stuGrade = 1
 stuLocation = "Ontario, Canada"
+# stuCurriculum = "pasted"
 template = template.format(grade = stuGrade, location = stuLocation, curriculum = data)
 # print(template)
-
 answer = deepseek_chain.invoke(template)
-print(answer)
+
+# clean the answer - just get the questions
+clean_answer = str(answer).split("</think>\n\n")[1]
+
+print(clean_answer)
+
+#list of q = list[print(clean_answer)]
+# list of ans = []
+
+# for loop where each question is asked
+    # ask user to input an answer
+    # chat bot has to check if the answer is correct
+        # if ans is CORRECT: move on to the next question
+        # if ans if WRONG: 
+            # brief explaination
+            # ask again with different number
+            # if the ans is CORRECT: move on
+
+# once for loop is done, they win the level
+
+
+
